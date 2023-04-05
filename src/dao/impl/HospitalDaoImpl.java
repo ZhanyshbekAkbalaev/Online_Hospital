89fb458404ad66c10a34dcbdfbe3fae5a546da70
Zhanyshbek <janyshbekakbalaev56@gmail.com>
2023-04-05 18:47:49 +0600

    Dao Hospital methods

--- src/dao/impl/HospitalDaoImpl.java
+++ src/dao/impl/HospitalDaoImpl.java
@@ -5,42 +5,69 @@ import database.Database;
 import models.Hospital;
 import models.Patient;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HospitalDaoImpl implements HospitalDao {
-    private List<Database> data;
+    private Database data;
 
     @Override
     public String addHospital(Hospital hospital) {
-        for (Database database : data) {
-            database.getHospitals().add(hospital);
-        }
         return "Succesfully add Hospital!";
     }
 
     @Override
     public Hospital findHospitalById(Long id) {
-        return null;
+        Hospital hospital1 = new Hospital();
+        for (Hospital hospital : data.getHospitals()) {
+            if (Objects.equals(hospital.getId(), id)) {
+                hospital1 = hospital;
+            } else {
+                System.out.println(hospital + "No!!!!!!!!!!!!!!!!!!");
+            }
+        }
+        return hospital1;
     }
 
     @Override
     public List<Hospital> getAllHospital() {
-        return null;
+        return data.getHospitals();
     }
 
     @Override
     public List<Patient> getAllPatientFromHospital(Long id) {
-        return null;
+        List<Patient> patients = new ArrayList<>();
+        for (Hospital hospital : data.getHospitals()) {
+            if (Objects.equals(hospital.getId(), id)) {
+                patients.addAll(hospital.getPatients());
+            } else {
+                System.out.println("No! PATIENTS");
+            }
+        }
+        return patients;
     }
 
     @Override
     public String deleteHospitalById(Long id) {
-        return null;
+        for (Hospital hospital : data.getHospitals()) {
+            if (Objects.equals(hospital.getId(), id)) {
+                data.getHospitals().remove(hospital);
+            } else {
+                return "No delete NO id!" + id;
+            }
+        }
+        return "Successfully remove hospita id" + id;
     }
 
     @Override
     public Map<String, Hospital> getAllHospitalByAddress(String address) {
-        return null;
+        Map<String, Hospital> getAll = new HashMap<>();
+        for (Hospital hospital : data.getHospitals()) {
+            if (hospital.getAddress().equals(address)) {
+                getAll.put(address, hospital);
+            } else {
+                System.out.println(address + "NO!! Address");
+            }
+        }
+        return getAll;
     }
 }
