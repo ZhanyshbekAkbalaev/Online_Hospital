6a711a878e3a9f02dbcc9a3d66342def74f4eeaf
Notnik_kg <j.temirlan141002@gmail.com>
2023-04-06 11:16:08 +0300

    Added Department DepartmentDao and DepartmentImpl

--- src/dao/impl/DoctorDao.java
+++ src/dao/impl/DoctorDao.java
@@ -9,8 +9,6 @@ public interface DoctorDao {
     Doctor findDoctorById(Long id);
     String updateDoctor(Long id, Doctor doctor);
     void deleteDoctorById(Long id);
-    // Department‘ти id менен табып, анан Doctor‘лорду листтеги айдилери менен табып анан ошол табылган
-    // Department'ге табылган Doctor'лорду кошуп коюнунуз
     String assignDoctorToDepartment(Long departmentId, List<Long> doctorsId);
     List<Doctor> getAllDoctorsByHospitalId(Long id);
     List<Doctor> getAllDoctorsByDepartmentId(Long id);
