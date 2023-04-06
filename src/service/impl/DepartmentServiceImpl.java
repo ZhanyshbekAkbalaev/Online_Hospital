4f3d11fbf7f9491e3488202adb686faf9d34bc04
Notnik_kg <j.temirlan141002@gmail.com>
2023-04-06 11:28:31 +0300

    Added DepartmentService and DepartmentServiceImpl

---
+++ src/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,33 @@
+package service.impl;
+
+import models.Department;
+import service.DepartmentService;
+
+import java.util.List;
+
+public class DepartmentServiceImpl implements DepartmentService {
+    @Override
+    public String addDepartmentToHospital(Long id, Department department) {
+        return null;
+    }
+
+    @Override
+    public List<Department> getAllDepartmentByHospital(Long id) {
+        return null;
+    }
+
+    @Override
+    public Department findDepartmentByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void deleteDepartmentById(Long id) {
+
+    }
+
+    @Override
+    public String updateDepartmentById(Long id, Department department) {
+        return null;
+    }
+}
