9d1e9bc7aba71cdaa28bec9042280286aa4e3327
kanymgul05032003 <abdubaitova05@mail.ru>
2023-04-06 13:43:12 +0600

    added  Patiant clss

---
+++ src/service/PatientService.java
@@ -0,0 +1,16 @@
+package service;
+
+import models.Patient;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PatientService {
+    String addPatientToHospital(Long id, Patient patient);
+    String addPatientsToHospital(Long id, List<Patient> patients);
+    String updatePatientById(Long id, Patient patientsNewInfo);
+    void removePatientById(Long id);
+    Patient getPatientById(Long id);
+    Map<Integer, Patient> getPatientByAge();
+    List<Patient> sortPatientsByAge(String ascOrDesc);
+}
---
+++ src/service/impl/PatientServiceImpl.java
@@ -0,0 +1,45 @@
+package service.impl;
+
+import models.Patient;
+import service.PatientService;
+
+import java.util.List;
+import java.util.Map;
+
+public class PatientServiceImpl implements PatientService {
+
+    @Override
+    public String addPatientToHospital(Long id, Patient patient) {
+        return null;
+    }
+
+    @Override
+    public String addPatientsToHospital(Long id, List<Patient> patients) {
+        return null;
+    }
+
+    @Override
+    public String updatePatientById(Long id, Patient patientsNewInfo) {
+        return null;
+    }
+
+    @Override
+    public void removePatientById(Long id) {
+
+    }
+
+    @Override
+    public Patient getPatientById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Patient> getPatientByAge() {
+        return null;
+    }
+
+    @Override
+    public List<Patient> sortPatientsByAge(String ascOrDesc) {
+        return null;
+    }
+}
