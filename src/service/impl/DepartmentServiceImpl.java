7e292aa559cd9d3fd72ff4916398cd35d14d6ea1
Notnik_kg <j.temirlan141002@gmail.com>
2023-04-06 12:32:11 +0300

    Write ServiceDao

--- src/service/impl/DepartmentServiceImpl.java
+++ src/service/impl/DepartmentServiceImpl.java
@@ -1,33 +1,35 @@
 package service.impl;
 
+import dao.DepartmentDao;
 import models.Department;
 import service.DepartmentService;
 
 import java.util.List;
 
 public class DepartmentServiceImpl implements DepartmentService {
+    private DepartmentDao dao;
     @Override
     public String addDepartmentToHospital(Long id, Department department) {
-        return null;
+        return dao.addDepartmentToHospital(id,department);
     }
 
     @Override
     public List<Department> getAllDepartmentByHospital(Long id) {
-        return null;
+        return dao.getAllDepartmentByHospital(id);
     }
 
     @Override
     public Department findDepartmentByName(String name) {
-        return null;
+        return dao.findDepartmentByName(name);
     }
 
     @Override
     public void deleteDepartmentById(Long id) {
-
+       dao.deleteDepartmentById(id);
     }
 
     @Override
     public String updateDepartmentById(Long id, Department department) {
-        return null;
+        return dao.updateDepartmentById(id,department);
     }
 }
