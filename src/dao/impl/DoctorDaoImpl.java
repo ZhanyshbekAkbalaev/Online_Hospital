98c291bd83e0737ae01f0f2527d9622149a54310
user <koshalieverkinbek@gmail.com>
2023-04-06 13:58:08 +0600

    Added  all method doctor class

--- src/dao/impl/DoctorDaoImpl.java
+++ src/dao/impl/DoctorDaoImpl.java
@@ -1,44 +1,117 @@
 package dao.impl;
 
 import database.Database;
+import models.Department;
 import models.Doctor;
+import models.Hospital;
 
+import javax.print.Doc;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class DoctorDaoImpl implements DoctorDao{
-    Database database = new Database();
+public class DoctorDaoImpl implements DoctorDao {
+    private Database database;
+
     @Override
     public String addDoctorToHospital(Long id, Doctor doctor) {
+        for (Hospital hosp : database.getHospitals()) {
+            if (hosp.getId() == id) {
+                hosp.getDoctors().add(doctor);
+                System.out.println(hosp.getDoctors() + "doctor added successfully ");
+
+            }
+        }
+
         return null;
     }
 
     @Override
     public Doctor findDoctorById(Long id) {
-        return null;
-    }
+        Doctor doctor1 = new Doctor();
+        for (Hospital hosp : database.getHospitals()) {
+            for (Doctor doctor : hosp.getDoctors()) {
+                if (doctor.getId()==id){
+                   doctor1 = doctor;
+                }else {
+                    System.out.println("no such id");
 
+                }
+            }
+        }
+        return doctor1;
+    }
     @Override
     public String updateDoctor(Long id, Doctor doctor) {
-        return null;
+        for (Hospital hosp : database.getHospitals()) {
+            for (Doctor hospDoctor : hosp.getDoctors()) {
+                if (hosp.getId() == id) {
+                    hospDoctor.setFirstName(doctor.getFirstName());
+                    hospDoctor.setLastName(doctor.getLastName());
+                }else {
+                    System.out.println("doctor not renamed");
+                }
+            }
+        }
+        return "Update doctor Successfully";
     }
-
     @Override
     public void deleteDoctorById(Long id) {
-
+        for (Hospital hosp : database.getHospitals()) {
+            for (Doctor doctor : hosp.getDoctors()) {
+                if (doctor.getId() == id) {
+                    hosp.getDoctors().remove(doctor);
+                } else {
+                    System.out.println("no id!");
+                }
+            }
+        }
     }
-
     @Override
     public String assignDoctorToDepartment(Long departmentId, List<Long> doctorsId) {
-        return null;
+        for (Hospital hosp : database.getHospitals()) {
+            for (Department department : hosp.getDepartments()) {
+                if (Objects.equals(department.getId(), departmentId)) {
+                    for (Doctor doctor : hosp.getDoctors()) {
+                        for (Long aLong : doctorsId) {
+                            if (Objects.equals(doctor.getId(), aLong)) {
+                                department.setDoctors(hosp.getDoctors());
+                            } else {
+                                return "No id:";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return "assign doctor to department Successfully!!!!!!!!!!!!!!";
     }
 
     @Override
     public List<Doctor> getAllDoctorsByHospitalId(Long id) {
-        return null;
+        List<Doctor> doctors = new ArrayList<>();
+        for (Hospital hospital : database.getHospitals()) {
+            if (hospital.getId() == id) {
+                doctors.addAll(hospital.getDoctors());
+            }else {
+                System.out.println("did not find a doctor");
+            }
+        }
+        return doctors;
     }
 
     @Override
     public List<Doctor> getAllDoctorsByDepartmentId(Long id) {
-        return null;
+        List<Doctor> doctors = new ArrayList<>();
+        for (Hospital hospital : database.getHospitals()) {
+            for (Department department : hospital.getDepartments()) {
+                if (Objects.equals(department.getId(), id)){
+                    doctors.addAll(department.getDoctors());
+                }else {
+                    System.out.println("did not add a doctor to the department");
+                }
+            }
+        }
+        return doctors;
     }
 }
